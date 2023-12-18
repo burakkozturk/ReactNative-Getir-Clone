6f9b728e4d35cd3de8bbf0b72a00b04015e6b908
burak <devx.burak@gmail.com>
2023-12-18 23:27:29 +0300

    Home Page Header Completed

--- src/screens/HomeScreen/styles.ts
+++ src/screens/HomeScreen/styles.ts
@@ -5,8 +5,35 @@
 
      headerMain:{
         height: height * 0.062 ,
-        backgroundColor:'red',
+        backgroundColor:'#ffd60a',
+        flexDirection:"row"
+     },
+     headerLeft:{
+        height: height* 0.062,
+        width:'80%',
+        backgroundColor:'white',
+        flexDirection:"row",
+        alignItems:'center',
+        paddingHorizontal:"5%",
+        borderTopRightRadius:22 ,
+        borderBottomRightRadius:22
+      },
+     image:{
+        width:30,
+        height:30
+     },
+     headerView:{
+        flexDirection:"row",
+        textAlign:"center",  
+        alignItems:"center"
+     },
+     headerRight:{
+        width:"35%",
+        paddingLeft:15,
+        justifyContent:'center',
+        alignItems:'center'
      }
+    
  })
 
- export default styles;
+ export default styles; 
