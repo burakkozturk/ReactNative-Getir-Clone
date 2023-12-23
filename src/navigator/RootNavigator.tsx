2bd5d8aeca0f5c54eefd8f66407bf0ff06090ad5
burak <devx.burak@gmail.com>
2023-12-23 20:48:33 +0300

    BottomTabNavigation

---
+++ src/navigator/RootNavigator.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+
+function RootNavigator() {
+  return (
+    <div>RootNavigator</div>
+  )
+}
+
+export default RootNavigator
